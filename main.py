10bac9ec331071f986c7d88e5512e3b73dff538e
Stefan R. Grotz <stefan.grotz@posteo.de>
2023-05-17 09:15:24 +0200

    wikidata query

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 import urllib.parse
 import requests
+import json
 
 class handler(BaseHTTPRequestHandler):
 
@@ -8,55 +9,45 @@ class handler(BaseHTTPRequestHandler):
         # Parse query parameters
         path, _, query_string = self.path.partition('?')
         params = urllib.parse.parse_qs(query_string)
-        title = params.get('title', [''])[0]  # Use the 'title' parameter value if it exists, or '' if it doesn't
+        query = params.get('query', [''])[0]  # Use the 'query' parameter value if it exists, or '' if it doesn't
 
-        if not title:
-            # Send an error message if the 'title' parameter is missing
+        if not query:
+            # Send an error message if the 'query' parameter is missing
             self.send_response(400)
             self.send_header('Content-type','text/plain; charset=utf-8')
             self.end_headers()
-            self.wfile.write('Error: Missing "title" parameter. Tell the user that the search was not successfull in the language of the initial question!'.encode('utf-8'))
+            self.wfile.write('Error: Missing "query" parameter. Please provide a SPARQL query.'.encode('utf-8'))
             return
 
-        # Fetch data from Wikipedia
-        response = requests.get('https://de.wikipedia.org/w/api.php', params={
-            'format': 'json',
-            'action': 'query',
-            'prop': 'extracts',
-            'exintro': '1',
-            'explaintext': '1',
-            'titles': title,
-        })
+        # Fetch data from Wikidata
+        response = requests.get(f"https://query.wikidata.org/bigdata/namespace/wdq/sparql?query={urllib.parse.quote(query)}", headers={"Accept": "application/sparql-results+json"})
         data = response.json()
 
-        # Extract the page extract text
-        pages = data['query']['pages']
-        page = next(iter(pages.values()))
-        extract = page.get('extract', '')
+        # Extract the results
+        results = data['results']['bindings']
 
-        if not extract:
-            # If the search did not return a text, find similar articles
-            response = requests.get('https://de.wikipedia.org/w/api.php', params={
-                'action': 'opensearch',
-                'profile': 'fuzzy',
-                'search': title,
-            })
-            data = response.json()
-
-            # Extract the titles of similar articles and format them as a markdown list
-            similar_articles = data[1]
-            similar_articles_markdown = "\n".join(f"- {title}" for title in similar_articles)
-
+        if not results:
             self.send_response(404)
             self.send_header('Content-type','text/plain; charset=utf-8')
             self.end_headers()
-            self.wfile.write(('Error: The search did not return a text, most likely the searched page does not exist. Here are some similar articles:\n\n' + similar_articles_markdown).encode('utf-8'))
+            self.wfile.write('Error: The query did not return any results.'.encode('utf-8'))
             return
+
+        # Format the results in markdown as a table
+        # Headers
+        headers = results[0].keys()
+        markdown_results = ["| " + " | ".join(headers) + " |", "| " + " | ".join("---" for _ in headers) + " |"]
+
+        # Rows
+        for result in results:
+            markdown_results.append("| " + " | ".join(result[var]['value'] for var in headers) + " |")
+
+        markdown_results_str = "\n".join(markdown_results)
 
         # Send the response
         self.send_response(200)
         self.send_header('Content-type','text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.write(extract.encode('utf-8'))
+        self.wfile.write(markdown_results_str.encode('utf-8'))
 
         return
